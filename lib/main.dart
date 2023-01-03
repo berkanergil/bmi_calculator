9aecc23473a673997ee8cc8811a0f4ae2ee02ed6
Your Name <you@example.com>
2023-01-03 19:37:19 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BMICalculator());
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
